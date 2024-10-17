82056601fac96754687b48958a9678fc200da997
Minh Hiếu <gaconmvp2312@gmail.com>
2024-10-17 21:57:16 +0700

    change video order in playlist

---
+++ api/Channel/src/main/java/com/vibio/channel/dto/request/UpdatePlaylistVideoOrderRequest.java
@@ -0,0 +1,9 @@
+/*
+ *  UpdatePlaylistVideoOrderRequest
+ *  @author: Minhhieuano
+ *  @created 10/17/2024 8:44 PM
+ * */
+
+
+package com.vibio.channel.dto.request;public class UpdatePlaylistVideoOrderRequest {
+}
